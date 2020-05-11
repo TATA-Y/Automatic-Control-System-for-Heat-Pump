8d2c15b1e6229547a30e60d19443ece797dde0de
TianY <50518391+Tianhao-Y@users.noreply.github.com>
2020-05-11 12:08:56 +0800

    Update hp_test.py

--- test/hp_test.py
+++ test/hp_test.py
@@ -3,14 +3,10 @@ from hp_model import operation
 init = 0
 last_q = init
 state = 1
-Q, steady, P_total, COP = operation(state, last_q, True, 'Cooler')
+Q, P_total, COP = operation(state, last_q, True, 'Cooler')
 
-if steady == 1:
-    steady = "Yes"
-else:
-    steady = "No"
+
 
 print("Thermal input to the Room: %10.3f" % Q + "\n",
-      "Does the heat pump enter a stable state: " + steady + "\n",
       "Total power consumption: %10.3f" % P_total + "\n",
       "Energy efficiency of heat pump: %10.3f" % COP)
